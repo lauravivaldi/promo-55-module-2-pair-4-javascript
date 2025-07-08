8f8a8f0dae3060bc9b55d5551539128711251207
BelenCB <belencolladobarrena@gmail.com>
2025-07-08 21:31:03 +0200

    ejercicios condicionales

--- main.js
+++ main.js
@@ -46,3 +46,33 @@ formButton.addEventListener("click", (event) => {
     helloText.textContent = "Hola " + helloName;
     form.appendChild(helloText);
 });
+
+// ejercicios condicionales
+
+const inputName = document.querySelector(".input-name").value;
+
+if (inputName === "Maria" || inputName === "Luisa" ) {
+    console.log("Bienvenida")
+}
+else {
+    console.log("Lo siento, pero el usuario que has introducido no está registrado")
+};
+
+
+function findNumber() {
+    const number = document.querySelector(".number").value; 
+    if (number === 0) {
+    console.log('El número es 0')
+    } else if (number < 0) {
+    console.log('El número es negativo')
+    } else if (number + 2 > 13 && number + 2 <= 20) {
+    console.log('El número más 2 es mayor que 13 pero menor o igual que 20')
+    } else if (number > 20 && number < 50) {
+    console.log('El número es mayor que 20 pero menor que 50')
+    } else {
+    console.log('el número no es 123123125')
+    }
+}
+
+const buttonNumber = document.querySelector(".button-number");
+buttonNumber.onclick = findNumber;
